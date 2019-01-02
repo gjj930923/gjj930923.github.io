500ded6a56e2d0078ff6352fe0e57ab6dbfd7aa4
Jeff Guo <895931778@qq.com>
2019-01-02 01:04:08 -0500

    Site updated: 2019-01-02 01:04:05

--- 2018/12/31/2018-summaries/watermark.sh
+++ 2018/12/31/2018-summaries/watermark.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+mogrify -quality 30 *.jpg
 for each in *.jpg
 do
 s=`du -k $each | awk '{print $1}'`
 if [ $s -gt 10 ]; then
-composite -gravity southeast ../icon.jpeg $each $each 2>/dev/null
+composite -gravity southeast ../../img/icon.jpeg $each $each 2>/dev/null
 echo "$each: done!"
 fi
 done
