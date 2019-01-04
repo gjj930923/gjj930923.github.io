417adccc7d71c300e0e46f372085f48c0f37bb53
Jeff Guo <895931778@qq.com>
2019-01-03 23:32:59 -0500

    Site updated: 2019-01-03 23:32:57

--- 2018/12/31/2018-summaries/watermark.sh
+++ 2018/12/31/2018-summaries/watermark.sh
@@ -4,7 +4,13 @@ for each in *.jpg
 do
 s=`du -k $each | awk '{print $1}'`
 if [ $s -gt 10 ]; then
-composite -gravity southeast ../../img/icon.jpeg $each $each 2>/dev/null
+w=`identify -format '%w' $each`
+h=`identify -format '%h' $each`
+min=w
+if [ $w -gt $h ]; then
+min=h
+fi
+composite -gravity southeast -geometry $((min / 10))x$((min / 10))+0+0 ../../img/icon.jpeg $each $each 2>/dev/null
 echo "$each: done!"
 fi
 done
